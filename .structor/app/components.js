7b8e4ae88502eb39445ecb0ad0385a5551b346d0
hojong6636 <hojong6636@gmail.com>
2018-06-28 03:35:09 +0900

    add Title component

--- .structor/app/components.js
+++ .structor/app/components.js
@@ -1,10 +1,7 @@
-import * as Layout from 'modules/Layout';
-import * as Router from 'modules/Router';
-import * as Animations from 'modules/Animations';
-import * as PortofolioCard from 'modules/PortofolioCard';
-export {
-    Layout,
-    Router,
-    Animations,
-    PortofolioCard
-};
+import * as Layout from "modules/Layout";
+import * as Router from "modules/Router";
+import * as Animations from "modules/Animations";
+import * as PortofolioCard from "modules/PortofolioCard";
+import * as Title from "modules/Title";
+
+export { Layout, Router, Animations, PortofolioCard, Title };
