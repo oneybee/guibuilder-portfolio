bd7d556ac99a2063eed7fa5ad5bf853b5d994254
hojong6636 <hojong6636@gmail.com>
2018-06-28 04:02:26 +0900

    edit Container view

--- .structor/app/components.js
+++ .structor/app/components.js
@@ -3,5 +3,6 @@ import * as Router from "modules/Router";
 import * as Animations from "modules/Animations";
 import * as PortofolioCard from "modules/PortofolioCard";
 import * as Title from "modules/Title";
+import * as Container from "modules/Container";
 
-export { Layout, Router, Animations, PortofolioCard, Title };
+export { Layout, Router, Animations, PortofolioCard, Title, Container };
