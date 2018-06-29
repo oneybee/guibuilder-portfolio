dedf0ebf454e208a3bef193ebb1a00394dce2e96
hojong6636 <hojong6636@gmail.com>
2018-06-30 04:32:25 +0900

    make work/project page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Page470 from "./Page470";
 import Page133 from "./Page133";
 import Page160 from "./Page160";
+import Page727 from "./Page727";
+import Page470 from "./Page470";
 
 class App extends React.Component {
   render() {
@@ -16,13 +17,8 @@ class App extends React.Component {
 export default {
   path: "/",
   component: App,
-  indexRoute: { component: Page470 },
+  indexRoute: { component: Page133 },
   childRoutes: [
-    {
-      path: "/portfolio",
-      name: "/portfolio",
-      component: Page470
-    },
     {
       path: "/get-in-touch",
       name: "/get-in-touch",
@@ -34,9 +30,19 @@ export default {
       component: Page160
     },
     {
+      path: "/work/chrome-os-system-menu",
+      name: "/work/chrome-os-system-menu",
+      component: Page727
+    },
+    {
+      path: "/portfolio",
+      name: "/portfolio",
+      component: Page470
+    },
+    {
       path: "*",
       name: "notfound",
-      component: Page470
+      component: Page133
     }
   ]
 };
