0d07918ac1c232645ee30fce0e824bc43deeb453
hojong6636 <hojong6636@gmail.com>
2018-06-30 05:15:08 +0900

    add text block on work/project header section

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Page133 from "./Page133";
-import Page160 from "./Page160";
 import Page727 from "./Page727";
+import Page160 from "./Page160";
+import Page133 from "./Page133";
 import Page470 from "./Page470";
 
 class App extends React.Component {
@@ -17,12 +17,12 @@ class App extends React.Component {
 export default {
   path: "/",
   component: App,
-  indexRoute: { component: Page133 },
+  indexRoute: { component: Page727 },
   childRoutes: [
     {
-      path: "/get-in-touch",
-      name: "/get-in-touch",
-      component: Page133
+      path: "/work/chrome-os-system-menu",
+      name: "/work/chrome-os-system-menu",
+      component: Page727
     },
     {
       path: "/work",
@@ -30,9 +30,9 @@ export default {
       component: Page160
     },
     {
-      path: "/work/chrome-os-system-menu",
-      name: "/work/chrome-os-system-menu",
-      component: Page727
+      path: "/get-in-touch",
+      name: "/get-in-touch",
+      component: Page133
     },
     {
       path: "/portfolio",
@@ -42,7 +42,7 @@ export default {
     {
       path: "*",
       name: "notfound",
-      component: Page133
+      component: Page727
     }
   ]
 };
